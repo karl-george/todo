384eb955074d47933a27b2c68d7d5e3077f6b327
Karl George <karlgdev@gmail.com>
2022-04-21 14:19:48 +0200

    seeding data

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    title: 'First todo',
+    isComplete: false,
+  },
+  {
+    id: 2,
+    title: 'Second todo',
+    isComplete: false,
+  },
+  {
+    id: 3,
+    title: 'Third todo',
+    isComplete: false,
+  },
+];
