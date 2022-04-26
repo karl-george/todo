0a742203de1fa750f2392014f6a625c05e3f84bc
Karl George <karlgdev@gmail.com>
2022-04-26 13:36:53 +0200

    Finishing styling tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,13 +3,15 @@ const Task = () => {
     <div>
       <div className='tasks'>
         <h3 className='task-title'>Do very important thing</h3>
-        <input type='checkbox' name='' id='' />
+        <input type='checkbox' name='isComplete' className="checkbox"/>
+        <label className="checkbox-label" htmlFor="isComplete">Complete the task</label>
       </div>
       <div className='tasks'>
         <h3 className='task-title completed'>
           Do very important and very long task
         </h3>
-        <input type='checkbox' name='' id='' />
+        <input type='checkbox' name='isComplete' className="checkbox"/>
+        <label className="checkbox-label" htmlFor="isComplete">Complete the task</label>
       </div>
     </div>
   );
